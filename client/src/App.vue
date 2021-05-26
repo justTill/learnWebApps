a18eabe80c89b9dae496a7e6b386a7f35846a5f2
Till Pilarczyk <till.pilarczyk@study.hs-duesseldorf.de>
2021-05-26 12:58:17 +0200

    first setup with dockercompose and basis server and client

---
+++ client/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <button v-on:click="test">Mein button</button>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  },
+  methods: {
+    test: async function () {
+      const res = await fetch('/api/v1');
+      console.log(res);
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
