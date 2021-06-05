9859e33db9aa2826902389ba99630e962ab2b0d9
Till Pilarczyk <till.pilarczyk@study.hs-duesseldorf.de>
2021-06-05 10:11:01 +0200

    fix liquibase

--- server/liquibase.js
+++ server/liquibase.js
@@ -1,18 +1,14 @@
-const Liquibase = require('liquibase').Liquibase;
-const POSTGRESQL_DEFAULT_CONFIG = require('liquibase').POSTGRESQL_DEFAULT_CONFIG;
-
-const myConfig = {
-    ...POSTGRESQL_DEFAULT_CONFIG,
+const liquibase = require("liquibase-wrapper");
+const config = {
     changeLogFile: './liquibase/changelog.json',
     url: 'jdbc:postgresql://' + process.env.DB_CONTAINER_NAME + ':5432/' + process.env.DB_Name,
     username: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
+    classpath: "postgres/postgresql-42.2.18.jar"
 }
-const instTs = new Liquibase(myConfig);
 
-instTs.status()
-    .then(r => {
-        instTs.update()
-            .then(console.log("Successfully run liquibase"))
-            .catch(e => console.log(e))
-    }).catch(e => console.log(e));
+liquibase(config).run("update")
+    .then(() => console.log('success'))
+    .catch((err) => {
+        console.log("Failed to run Liquibase update: ", err);
+    });
