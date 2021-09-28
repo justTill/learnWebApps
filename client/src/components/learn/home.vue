6a97c357ae52c27c7b81c2bf8db8cf218f82332b
Till Pilarczyk <till.pilarczyk@study.hs-duesseldorf.de>
2021-09-28 23:04:36 +0200

    typo

--- client/src/components/learn/home.vue
+++ client/src/components/learn/home.vue
@@ -28,7 +28,7 @@
       (<img src="../../assets/info.png" style="padding-bottom: 5px">)
       sowie Aufgaben (<img src="../../assets/lessons.png" style="padding-bottom: 5px">) beinhalten.
       <br>
-      Wenn du in der Kapitel oder Unterthemaansicht bisst kann du mit den Buttons
+      Wenn du in der Kapitel oder Unterthemaansicht bist kann du mit den Buttons
       (
       <img src="../../assets/skip-prev.svg" style="padding-bottom: 5px"> /
       <img src="../../assets/skip-next.svg" style="padding-bottom: 5px">
